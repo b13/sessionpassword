d326e60c75cde7829f2f9f407662cc84c7a6fbb1
Achim Fritz <af@achimfritz.de>
2025-05-21 13:01:54 +0200

    [TASK] rm useless service

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,10 +13,3 @@ defined('TYPO3') or die();
     [\B13\Sessionpassword\Controller\PasswordController::class => 'unlock']
 );
 
-// hook to see if the content object is allowed to render anything at all,
-// based on the data inside the cObj
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass']['sessionpassword'] = [
-    'sessionpassword',    // key for the array
-    \B13\Sessionpassword\Service\ContentObjectService::class,    // class to instantiate
-];
-
